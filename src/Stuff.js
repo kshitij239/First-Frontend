4d6fef172eb35a154f8fcf16df7225032c835f2a
kshitij239 <76518826+kshitij239@users.noreply.github.com>
2021-04-18 14:35:35 +0530

    Update Stuff.js

--- src/Stuff.js
+++ src/Stuff.js
@@ -10,7 +10,7 @@ class Stuff extends Component {
         <li>I live in Mumbai</li>
           <li>Just one other club -Instrumentation Forum</li>
           
-          <li>Interests-Sports(cricket,badminton,basketball),reading about Astronomy related stuff,trying to find interest in coding as well.</li>
+          <li>Interests-Sports(cricket,badminton,basketball),Astronomy related stuff,a bit of coding as well.</li>
           <li>Link for a small project i tried on <a href="http://kshitij2002san.pythonanywhere.com/">Django</a> backend </li>
         
         </ol>
